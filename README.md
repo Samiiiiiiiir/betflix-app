726328bc92d9b6f41a8eed3f07963d1a718e5530
Samir <sam1r.aslanoff.as@gmail.com>
2025-02-28 17:43:37 +0200

    added actors and links to imdb/kinopoisk in movie detail component

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Currently, two official plugins are available:
 
 https://www.youtube.com/watch?v=inReuj1yPjU&t=28s
 
-4:43:30
+5:41:50
 
 (на будущее добавить страницу в url с номером страницы. например /comics/3 чтоб можно было перейти сразу)
--- src/components/pages/MovieDetail/MovieDetail.jsx
+++ src/components/pages/MovieDetail/MovieDetail.jsx
@@ -1,5 +1,11 @@
 import { ArrowBack } from '@mui/icons-material';
-import { Button, CircularProgress, Stack, Typography } from '@mui/material';
+import {
+  Button,
+  ButtonGroup,
+  CircularProgress,
+  Stack,
+  Typography,
+} from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import React from 'react';
 import { useNavigate, useParams } from 'react-router';
@@ -31,83 +37,108 @@ export const MovieDetail = () => {
   if (errorStatus) return <ErrorMessage />;
 
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        paddingBlock: '20px',
-      }}
-    >
-      <Grid item size={4}>
-        <img
-          src={filmData.posterUrlPreview}
-          alt={filmData.nameRu}
-          width="100%"
-          loading="lazy"
-        />
-      </Grid>
-      <Grid item size={6}>
-        <Grid
-          container
-          sx={{
-            alignItems: 'center',
-            paddingBlock: '8px',
-          }}
-        >
-          <Grid size={4}>
-            <Button
-              startIcon={<ArrowBack />}
-              size="medium"
-              onClick={() => navigate(-1)}
-            >
-              Назад
+    <>
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          paddingBlock: '20px',
+        }}
+      >
+        <Grid item size={4}>
+          <img
+            src={filmData.posterUrlPreview}
+            alt={filmData.nameRu}
+            width="100%"
+            loading="lazy"
+          />
+          <ButtonGroup
+            variant="outlined"
+            size="small"
+            sx={{ justifyContent: 'center', width: '100%' }}
+          >
+            <Button target="_blank" href={filmData.webUrl}>
+              Кинопоиск
             </Button>
+            <Button
+              target="_blank"
+              href={`https://www.imdb.com/title/${filmData.imdbId}`}
+            >
+              IMDB
+            </Button>
+          </ButtonGroup>
+        </Grid>
+        <Grid item size={6}>
+          <Grid
+            container
+            sx={{
+              alignItems: 'start',
+              paddingBottom: '8px',
+            }}
+          >
+            <Grid size={4}>
+              <Button
+                startIcon={<ArrowBack />}
+                size="medium"
+                onClick={() => navigate(-1)}
+              >
+                Назад
+              </Button>
+            </Grid>
+            <Grid size={8}>
+              <Typography variant="h5">{filmData.nameRu}</Typography>{' '}
+            </Grid>
           </Grid>
-          <Grid size={8}>
-            <Typography variant="h5">{filmData.nameRu}</Typography>{' '}
+          <Grid container rowSpacing={2}>
+            <Grid size={6}>Год</Grid>
+            <Grid size={6}>{filmData.year}</Grid>
+            <Grid size={6}>Страна</Grid>
+            <Grid size={6}>
+              {filmData.countries
+                .reduce((acc, { country }) => acc + country + ', ', '')
+                .slice(0, -2)}
+            </Grid>
+            <Grid size={6}>Жанр</Grid>
+            <Grid size={6}>
+              {filmData.genres
+                .reduce((acc, { genre }) => acc + genre + ', ', '')
+                .slice(0, -2)}
+            </Grid>
+            <Grid size={6}>Режиссеры</Grid>
+            <Grid size={6}>
+              {staffData
+                .filter((item) => item.professionText == 'Режиссеры')
+                .reduce((acc, { nameRu }) => acc + nameRu + ', ', '')
+                .slice(0, -2)}
+            </Grid>
+            <Grid size={6}>Длительность</Grid>
+            <Grid size={6}>
+              {filmData.filmLength
+                ? `${filmData.filmLength} минут`
+                : 'Нет данных'}
+            </Grid>
+            <Grid size={12}>
+              <Typography gutterBottom>Описание</Typography>
+              <Typography>
+                {filmData.description
+                  ? filmData.description
+                  : 'Описание отсутствует'}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid container rowSpacing={2}>
-          <Grid size={6}>Год</Grid>
-          <Grid size={6}>{filmData.year}</Grid>
-          <Grid size={6}>Страна</Grid>
-          <Grid size={6}>
-            {filmData.countries
-              .reduce((acc, { country }) => acc + country + ', ', '')
-              .slice(0, -2)}
-          </Grid>
-          <Grid size={6}>Жанр</Grid>
-          <Grid size={6}>
-            {filmData.genres
-              .reduce((acc, { genre }) => acc + genre + ', ', '')
-              .slice(0, -2)}
-          </Grid>
-          <Grid size={6}>Режиссеры</Grid>
-          <Grid size={6}>
-            {staffData
-              .filter((item) => item.professionText == 'Режиссеры')
-              .reduce((acc, { nameRu }) => acc + nameRu + ', ', '')
-              .slice(0, -2)}
-          </Grid>
-          <Grid size={6}>Длительность</Grid>
-          <Grid size={6}>
-            {filmData.filmLength
-              ? `${filmData.filmLength} минут`
-              : 'Нет данных'}
-          </Grid>
-          <Grid size={12}>
-            <Typography gutterBottom>Описание</Typography>
-            <Typography>
-              {filmData.description
-                ? filmData.description
-                : 'Описание отсутствует'}
-            </Typography>
-          </Grid>
+        <Grid item size={2}>
+          <Typography variant="h6">В главных ролях</Typography>
+          {staffData
+            .filter((item) => item.professionText == 'Актеры')
+            .slice(0, 9)
+            .map(({ nameRu }) => (
+              <Typography key={nameRu} gutterBottom>
+                {nameRu}
+              </Typography>
+            ))}
         </Grid>
       </Grid>
-      <Grid item size={2}>
-        Actors
-      </Grid>
-    </Grid>
+    </>
   );
 };
