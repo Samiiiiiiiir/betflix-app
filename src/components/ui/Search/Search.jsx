e3701e43d7aeb6d671e2b4d05100ec03b3f4ab5a
Samir <sam1r.aslanoff.as@gmail.com>
2025-03-01 11:14:58 +0200

    finished search component

--- src/components/ui/Search/Search.jsx
+++ src/components/ui/Search/Search.jsx
@@ -1,13 +1,40 @@
-import { Autocomplete, TextField } from '@mui/material';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import {
+  Autocomplete,
+  CircularProgress,
+  Stack,
+  TextField,
+} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { setSearchQuery } from '../../../features/searchQuerySlice';
 import { useGetFilmsQuery } from '../../../services/kinopoiskApi';
 
+const movieTypes = {
+  FILM: 'Фильм',
+  TV_SERIES: 'Сериал',
+  TV_SHOW: 'ТВ-шоу',
+  MINI_SERIES: 'Мини-сериал',
+};
+
 const Search = () => {
+  const [input, setInput] = useState('');
+
+  const dispatch = useDispatch();
+
   const { countries, genreId, order, type, year, keyword } = useSelector(
     (state) => state.searchQuery,
   );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      dispatch(setSearchQuery(input));
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [input]);
 
   const { data, isLoading, isFetching } = useGetFilmsQuery({
     countries,
@@ -18,15 +45,35 @@ const Search = () => {
     keyword,
   });
 
-  // if (isLoading || isFetching) return <div>Loading...</div>;
-
   return (
     <Autocomplete
       sx={{ width: '300px' }}
       style={{ backgroundColor: '#fff' }}
       freeSolo
-      options={data ? data.items.map((option) => option.nameRu) : []}
-      renderInput={(params) => <TextField {...params} />}
+      options={
+        data
+          ? data.items.map(
+              (option) =>
+                `${option.nameRu} - ${movieTypes[option.type]} (${option.year})`,
+            )
+          : []
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {isFetching && <CircularProgress size="2rem" />}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+      onInputChange={(_, value) => {
+        setInput(value);
+      }}
     />
   );
 };
